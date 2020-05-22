48d8addcdbe346e71769e4e1fc6f9f2661431e55
chenkj <chenkjia@live.com>
2020-05-22 10:26:47 +0800

    信号测试

--- app/service/tech/techStage.js
+++ app/service/tech/techStage.js
@@ -6,13 +6,13 @@ const { slice, maxBy, minBy, findIndex } = require('lodash');
 const pointFormat = (dayline, day) => {
   const tmp = dayline.reduce((result, item, index) => {
     const part = slice(dayline, index >= day ? index - day : 0, index + day > dayline.length ? dayline.length : index + day);
-    const max = maxBy(part, ({ orgin }) => orgin.high);
-    const min = minBy(part, ({ orgin }) => orgin.low);
+    const max = maxBy(part, ({ adj }) => adj.high);
+    const min = minBy(part, ({ adj }) => adj.low);
     if (max.date === item.date) {
-      return [ ...result, { type: 'vertex', date: item.date, price: item.orgin.high }];
+      return [ ...result, { type: 'vertex', date: item.date, price: item.adj.high }];
     }
     if (min.date === item.date) {
-      return [ ...result, { type: 'nadir', date: item.date, price: item.orgin.low }];
+      return [ ...result, { type: 'nadir', date: item.date, price: item.adj.low }];
     }
     return result;
   }, []);
@@ -38,7 +38,7 @@ const stageFormat = (dayline, scale = 'large') => {
   return points.map((point, index) => {
     const nextPoint = points[index + 1] ? points[index + 1] : {
       date: dayline[dayline.length - 1].date,
-      price: dayline[dayline.length - 1].orgin.close,
+      price: dayline[dayline.length - 1].adj.close,
     };
     const start_index = findIndex(dayline, [ 'date', point.date ]);
     const end_index = findIndex(dayline, [ 'date', nextPoint.date ]);
@@ -54,9 +54,45 @@ const stageFormat = (dayline, scale = 'large') => {
       end_index,
       multiple: nextPoint.price / point.price,
       days: end_index - start_index,
-      children: scale === 'large' ? stageFormat(slice(dayline, start_index, end_index), 'middle') : [],
+      children: index >= points.length - 3 && scale === 'large' ? stageFormat(slice(dayline, start_index, end_index), 'middle') : [],
     };
   }).filter(item => item);
+};
+const isStageSign = stages => {
+  // 如果趋势小于2，则返回错误
+  if (stages.length < 2) {
+    return false;
+  }
+  // 定义当前大趋势与上一个大趋势,与当前中趋势
+  const currentStage = stages[stages.length - 1];
+  // 当前趋势的子趋势为3个
+  if (currentStage.children.length !== 3) {
+    return false;
+  }
+  // 定义当前中趋势
+  const currentMiddleStage = currentStage.children[currentStage.children.length - 1];
+  // 当前趋势与当前子趋势都为rise
+  if (!(currentStage.type === 'rise' && currentMiddleStage.type === 'rise')) {
+    return false;
+  }
+  // 定义当上上个中趋势
+  const lastMiddleStage = currentStage.children[currentStage.children.length - 3];
+  // 当前趋势的结束价格大于上上个子趋势的结束价格
+  if (currentMiddleStage.end_price < lastMiddleStage.end_price) {
+    return false;
+  }
+  // 当前趋势的开始价格大于上上个子趋势的开始价格
+  if (currentMiddleStage.start_price < lastMiddleStage.start_price) {
+    return false;
+  }
+  // 上上个子趋势的结束价格大于上个大级别最后一个子趋势的开始价格
+  const lastStage = stages[stages.length - 2];
+  const lastLargeStageLastStage = lastStage.children[lastStage.children.length - 1];
+  if (lastMiddleStage.end_price < lastLargeStageLastStage.start_price) {
+    return false;
+  }
+  return true;
+
 };
 
 class TechStageService extends Service {
@@ -138,12 +174,8 @@ class TechStageService extends Service {
     const dayline = stock.dayline.reverse();
     const sign = dayline.reduce((result, item, i) => {
       const stages = stageFormat(slice(dayline, 0, i));
-      const lastStage = stages[stages.length - 1];
-      if (lastStage && lastStage.type === 'rise' && lastStage.children.length === 3) {
-        const lastFailStage = stages[stages.length - 2];
-        if (lastFailStage && lastFailStage.children[lastFailStage.children.length - 2] && lastFailStage.children[lastFailStage.children.length - 2].start_price < lastStage.children[0].end_price) {
-          return [ ...result, item ];
-        }
+      if (isStageSign(stages)) {
+        return [ ...result, item ];
       }
       console.log(`${stock.code}第${i}天测试`);
       return result;
